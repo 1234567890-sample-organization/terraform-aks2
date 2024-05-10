9e4fdf98ed014151e7b7d2f3d506712dd06ec3a9
sachin-learning-1 <sachin.learning.30@gmail.com>
2024-05-10 12:22:05 +0530

    Reverted Output file to declare the sensitive for few output varaiables only Corrected Terraform Formatting command in workflow files

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -6,26 +6,22 @@ output "id" {
 }
 
 output "kube_config" {
-  value     = azurerm_kubernetes_cluster.cluster.kube_config_raw
+  value = azurerm_kubernetes_cluster.cluster.kube_config_raw
   sensitive = true
 }
 
 output "client_key" {
-  value     = azurerm_kubernetes_cluster.cluster.kube_config.0.client_key
-  sensitive = true
+  value = azurerm_kubernetes_cluster.cluster.kube_config.0.client_key
 }
 
 output "client_certificate" {
-  value     = azurerm_kubernetes_cluster.cluster.kube_config.0.client_certificate
-  sensitive = true
+  value = azurerm_kubernetes_cluster.cluster.kube_config.0.client_certificate
 }
 
 output "cluster_ca_certificate" {
-  value     = azurerm_kubernetes_cluster.cluster.kube_config.0.cluster_ca_certificate
-  sensitive = true
+  value = azurerm_kubernetes_cluster.cluster.kube_config.0.cluster_ca_certificate
 }
 
 output "host" {
-  value     = azurerm_kubernetes_cluster.cluster.kube_config.0.host
-  sensitive = true
+  value = azurerm_kubernetes_cluster.cluster.kube_config.0.host
 }
