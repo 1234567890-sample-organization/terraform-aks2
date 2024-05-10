9c217513e6b43fc4531d90f1518675b6a050a819
sachin-learning-1 <sachin.learning.30@gmail.com>
2024-05-10 12:32:15 +0530

    Corrected Terraform Version as per region

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,15 +5,14 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.0.2"
+      version = "3.67.0"
     }
   }
 
-  required_version = ">= 1.1.0"
+  required_version = ">= 0.14"
 
   backend "azurerm" {
   }
-
 }
 
 provider "azurerm" {
